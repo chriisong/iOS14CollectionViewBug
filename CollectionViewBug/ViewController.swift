5212c946c74356cbc88d9a9ac2d688bfc94fb459
Chris Song <chris.dcsong@gmail.com>
2020-09-17 15:45:58 -0400

    Initial Commit

---
+++ CollectionViewBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewBug
+//
+//  Created by Chris Song on 2020-09-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
